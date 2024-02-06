05b49be275976f89171e1b143128a3ae546824d6
sarahesbie <3242982-sarahesbie@users.noreply.replit.com>
2024-02-06 01:54:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+original_text = "Hey there! "
+shouting_text = original_text.upper() # the .upper() method is used to convert the string to uppercase
+annoying_text = original_text * 10 # the * operator is used to repeat the string
+
+print(f"My annoying text is: {annoying_text}\n") # \n creates a new line
+print(f"It has {len(annoying_text)} characters.")
